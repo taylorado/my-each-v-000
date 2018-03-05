d309463c6dbc350e6e31659b847f2b631c9ca20a
Taylor Donlan <taylordonlan@gmail.com>
2018-03-05 17:09:36 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,6 +1,5 @@
 def my_each(array)
-#  if block_given?
-puts yield array[i]
+  if block_given?
     i=0
     while i < array.length
        i+=1
@@ -8,8 +7,8 @@ puts yield array[i]
 
      end
     array
-#  else
-#    puts "Hey! no Block was given!"
-#  end
+  else
+    puts "Hey! no Block was given!"
+  end
 
 end
